a688983ff99be1867f0dacdfd5d31203fd918c81
ljm <ljm3307@163.com>
2020-08-13 11:03:12 +0800

    消息列表

--- xxyshh/untitled3/myproject/src/workspace/Recycle.vue
+++ xxyshh/untitled3/myproject/src/workspace/Recycle.vue
@@ -7,9 +7,10 @@
     </el-header>
     <el-container>
       <el-aside width="15%">
-      <SideBar  currentindex='5'></SideBar>
+        <SideBar currentindex="5"></SideBar>
       </el-aside>
       <el-main style="width: 80%">
+        <delete-all style="float:right;margin-top:40px;margin-right=40px"></delete-all>
         <h2 class="h2color">回收站</h2>
         <!-- <doc-list :docs="recycleDocs"></doc-list> -->
         <doc-list :docs="Docs"></doc-list>
@@ -24,33 +25,44 @@ import NavBar from "../components/NavBar";
 import SideBar from "./SideBar";
 import DocList from "./DocList";
 import RightBar from "./RightBar";
+import DeleteAll from "./DeleteAll";
 
 export default {
   name: "Recycle",
-  components: { NavBar, SideBar,DocList,RightBar },
+  components: {
+    NavBar,
+    SideBar,
+    DocList,
+    RightBar,
+    DeleteAll,
+  },
   data() {
     return {
       headUrl: require("../assets/head.jpg"),
       Docs: [
         {
-          id: '1',
+          id: "1",
           title: "第111",
         },
         {
-          id: '2',
+          id: "2",
           title: "第二个hhhhh文档",
         },
         {
-          id: '3',
+          id: "3",
           title: "第三个文dashdkjlashdjkl档",
-        }
-      ]
+        },
+        {
+          id: "4",
+          title: "sisisi",
+        },
+      ],
     };
   },
 };
 </script>
 <style>
-  .h2color {
-    color: #7093FF;
-  }
+.h2color {
+  color: #7093ff;
+}
 </style>
---
+++ xxyshh/untitled3/myproject/src/workspace/message/MesList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="docs">
+    <mes-list-item v-for="item in mess" :mes-item="item" :key="item.id">
+    </mes-list-item>
+  </div>
+</template>
+
+<script>
+  import MesListItem from "./MesListItem";
+
+  export default {
+    name: "MesList",
+    components: {
+      MesListItem
+    },
+    props: {
+      //文档数组
+      mess: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .docs {
+    display: flex; 
+    flex-wrap: wrap;
+    padding: 10px;
+  }
+</style>
